920e2c918a5250374b97afce4a771aa8261b7730
Mingli Yuan <mingli.yuan@gmail.com>
2011-12-13 23:25:24 +0800

    change logo position and add margin

--- javascripts/milestones.js
+++ javascripts/milestones.js
@@ -1,4 +1,4 @@
-function load(cbMainseq, cbMilestone) {
+function load(cbMainseq) {
     var opts = {
         lines: 12, // The number of lines to draw
         length: 7, // The length of each line
@@ -16,8 +16,9 @@ function load(cbMainseq, cbMilestone) {
         counter++;
         if (counter===6) {
             spinner.stop();
+            logo();
+            cbMainseq();
             $('#mainseq').css('z-index', '-100');
-            cbMainseq(cbMilestone);
         }
     });
 
@@ -53,55 +54,57 @@ function color(i) {
 function mainseq(cbMilestone) {
     var canvas = document.getElementById("mainseq");
     var context = canvas.getContext("2d");
-    canvas.width = 1024;
+    canvas.width = 960;
     canvas.height = 512;
     for(var i = 0; i< 1024; i++) {
         context.fillStyle = color(i);
-        context.fillRect(i, 512 - Math.round(i * i * i * i / 2 / 1024 / 1024 / 1024), 10, 10);
+        context.fillRect(i - 110, 512 - Math.round(i * i * i * i / 2 / 1024 / 1024 / 1024), 10, 10);
     }
+    $('#star').css('top', '420px');
+    $('#star').css('left', '390px');
+    $('#planet').css('top', '410px');
+    $('#planet').css('left', '620px');
+    $('#weather').css('top', '280px');
+    $('#weather').css('left', '620px');
+    $('#plant').css('top', '280px');
+    $('#plant').css('left', '780px');
+    $('#animal').css('top', '160px');
+    $('#animal').css('left', '720px');
+    $('#evolve').css('top', '120px');
+    $('#evolve').css('left', '870px');
+}
+
+function logo() {
+    var canvas = document.getElementById("logo");
+    var context = canvas.getContext("2d");
+    canvas.width = 160;
+    canvas.height = 160;
 
     context.beginPath();
-    context.arc(75, 242, 49, 0, 2 * Math.PI, false);
+    context.arc(74, 72, 49, 0, 2 * Math.PI, false);
     context.strokeStyle = "#ffffff";
     context.stroke();
     context.closePath();
 
     context.beginPath();
-    context.arc(40, 210, 7, 0, 2 * Math.PI, false);
+    context.arc(39, 40, 7, 0, 2 * Math.PI, false);
     context.fillStyle = "#ffff88";
     context.fill();
     context.closePath();
 
     context.beginPath();
-    context.arc(80, 290, 10, 0, 2 * Math.PI, false);
+    context.arc(79, 120, 10, 0, 2 * Math.PI, false);
     context.fillStyle = "#8888ff";
     context.fill();
     context.closePath();
 
     context.beginPath();
-    context.arc(122, 230, 5, 0, 2 * Math.PI, false);
+    context.arc(121, 60, 5, 0, 2 * Math.PI, false);
     context.fillStyle = "#888888";
     context.fill();
     context.closePath();
-
-    cbMilestone();
-}
-
-function milestone() {
-    $('#star').css('top', '420px');
-    $('#star').css('left', '500px');
-    $('#planet').css('top', '410px');
-    $('#planet').css('left', '730px');
-    $('#weather').css('top', '280px');
-    $('#weather').css('left', '730px');
-    $('#plant').css('top', '280px');
-    $('#plant').css('left', '890px');
-    $('#animal').css('top', '160px');
-    $('#animal').css('left', '830px');
-    $('#evolve').css('top', '120px');
-    $('#evolve').css('left', '980px');
 }
 
 $(function() {
-    load(mainseq, milestone);
+    load(mainseq);
 });
